4208ffe08460619303efb10a7008f41a429c138b
sgiampietro <sofia.giampietro@inf.ethz.ch>
2025-03-31 10:56:26 +0200

    minor

--- lib/theory/src/Theory/Tools/EquationStore.hs
+++ lib/theory/src/Theory/Tools/EquationStore.hs
@@ -86,7 +86,7 @@ import qualified Control.Monad.State   as MS
 import           Data.Binary
 import qualified Data.Foldable         as F
 import           Data.List          (delete,find,intersect,intersperse,nub,(\\), permutations)
-import Data.Maybe ( fromJust, isJust )
+import Data.Maybe ( fromJust, isJust, isNothing )
 import qualified Data.Set              as S
 import           Extension.Data.Label  hiding (for, get)
 import qualified Extension.Data.Label  as L
@@ -751,8 +751,13 @@ addDHProtoEqs hnd allevars t1zzs permt zzbool eqdhstore = do
         --muvariablesindt = (concatMap varInMu permt)
         --ist1var x = elem x $ concatMap varsVTerm t1
         --isindtvar x = elem x $ concatMap varsVTerm permt
+        eqst1 = zipWith Equal permt t1
+        splitBPeqs = newBPeqs eqst1
+        newlist = case splitBPeqs of 
+                        Nothing -> []
+                        Just ts -> ts
 -- TODO: need to add change the "zipWith" Equal in the next line, with the newBPeqs function above!
-    case (if (any (uncurry notUnifiableLits) (zip permt t1)) then [] else unifyLNDHProtoTermFactored (zipWith Equal permt t1) `runReader` hnd) of
+    case (if ((any (uncurry notUnifiableLits) (zip permt t1)) || isNothing splitBPeqs) then [] else unifyLNDHProtoTermFactored newlist `runReader` hnd) of
         [] | zzbool ->  trace (show "amIhere?") $ return (set eqsConj falseEqConstrConj eqdhstore, Nothing)
         [] | not zzbool -> trace (show ("GENERALIZING", permt, t1)) $ addDHProtoEqs hnd allevars (map (\(t1,t1zz,zz) -> (t1zz,t1zz,zz)) t1zzs) permt True eqdhstore
         [substFresh] | substFresh == emptySubstVFresh ->
